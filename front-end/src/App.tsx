bd09f757ec15301fdb30fcc523fbfce842a4c175
mbaglieri <matiasbaglieri@gmail.com>
2022-07-31 12:59:00 -0300

    front-end: adding data in the home
    i added the basic data in the home

--- front-end/src/App.tsx
+++ front-end/src/App.tsx
@@ -1,15 +1,25 @@
-import React from 'react';
+import { useEffect } from 'react';
 import './App.css';
 
+
 function App() {
+  
+  useEffect(() => {
+    async function load() {
+     
+    }
+    
+    load();
+   }, []);
   return (
     <>
       <div className='faucet-wrapper'>
         <div className='faucet'>
-
+        Your account is: 
           <div className='balance-view is-size-2'>
             Current Balance: <strong>10</strong>ETH
           </div>
+          <button className='btn mr2'>login </button>
         </div>
       </div>
     </>
